ac3dac356bc256cdd1b1d13121bd3d934f37df85
electricboy6 <colterhardy@gmail.com>
2023-10-07 12:03:38 -0500

    added auto code for red side

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/StageRedLeft.java
@@ -0,0 +1,52 @@
+package org.firstinspires.ftc.teamcode.Autonomous;
+
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+import org.firstinspires.ftc.teamcode.drive.MecanumDriveBase;
+import org.firstinspires.ftc.teamcode.trajectorysequence.TrajectorySequence;
+
+@Autonomous
+public class StageRedLeft extends LinearOpMode {
+    static final double FEET_PER_METER = 3.28084;
+
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        MecanumDriveBase drive = new MecanumDriveBase(hardwareMap);
+
+        waitForStart();
+
+        drive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        // Start of Roadrunner stuff
+        Pose2d startPos = new Pose2d(60, 24, Math.toRadians(90));
+
+        Pose2d StageRedLeft= new Pose2d(30,12,Math.toRadians(90));
+        Pose2d StageRedCenter = new Pose2d(21.5,24,Math.toRadians(90));
+        Pose2d StageRedRight = new Pose2d(30,24,Math.toRadians(90));
+
+
+        Pose2d StageRedCenterDropoff = new Pose2d(34, 60, Math.toRadians(90));
+        Pose2d StageRedLeftDropoff = new Pose2d(29, 60, Math.toRadians(90));
+        //Pose2d StageRedRightDropoff = new Pose2d(34, 60, Math.toRadians(90));
+
+        Pose2d RedPark = new Pose2d(12,60,Math.toRadians(90));
+
+        drive.setPoseEstimate(startPos);
+
+        TrajectorySequence traj1 = drive.trajectorySequenceBuilder(startPos)
+                .strafeLeft(27)
+                .waitSeconds(1)
+                .back(4)
+                .lineToLinearHeading(StageRedLeftDropoff)
+                .waitSeconds(1)
+                .lineToLinearHeading(RedPark)
+                .build();
+
+        drive.followTrajectorySequence(traj1);
+    }
+}
