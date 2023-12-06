89ff7facfd5ca3887589a014bd54f54c8d136811
electricboy6 <colterhardy@gmail.com>
2023-12-05 20:02:12 -0600

    push changed ROIs

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Pipelines/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Pipelines/Constants.java
@@ -18,12 +18,12 @@ public class Constants {
             new Point(235, 145)
     );
     public static final Rect LEFT_ROI_BLUE = new Rect(
-            new Point(45, 75),
-            new Point(120, 150)
+            new Point(60, 90),
+            new Point(140, 175)
     );
     public static final Rect CENTER_ROI_BLUE = new Rect(
-            new Point(220, 60),
-            new Point(275, 120)
+            new Point(230, 70),
+            new Point(295, 140)
     );
     public static final Rect RIGHT_ROI_BLUE = new Rect(
             new Point(25, 70),
