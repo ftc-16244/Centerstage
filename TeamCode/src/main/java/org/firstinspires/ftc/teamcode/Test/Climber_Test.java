81edb9c3d95332b03186e70fc87046a0ddfefa63
David Atkinon <davidatkinson549@gmail.com>
2023-12-08 23:58:54 -0600

    diabled test code for meet 2

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test/Climber_Test.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test/Climber_Test.java
@@ -4,6 +4,7 @@ import com.acmerobotics.dashboard.FtcDashboard;
 import com.acmerobotics.dashboard.config.Config;
 import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
 import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
@@ -15,7 +16,7 @@ import org.firstinspires.ftc.teamcode.subsytems.Climber_Drone;
 
 @Config
 @TeleOp(group = "Teleop")
-
+@Disabled
 public class Climber_Test extends LinearOpMode {
 // test
 
