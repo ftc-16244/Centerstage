2de4fdba48922ef2ae96f16c9b73f8ed7fa6787a
Ritika Rudrapati <ritikarudrapati@gmail.com>
2024-02-15 18:44:39 -0600

    started a new auto file

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepStageRed_Center.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepStageRed_Center.java
@@ -1,21 +1,12 @@
 package com.example.meepmeeptesting;
 
 import com.acmerobotics.roadrunner.geometry.Pose2d;
-import com.acmerobotics.roadrunner.geometry.Vector2d;
 import com.noahbres.meepmeep.MeepMeep;
-import com.noahbres.meepmeep.core.colorscheme.scheme.ColorSchemeBlueDark;
-import com.noahbres.meepmeep.core.colorscheme.scheme.ColorSchemeRedDark;
 import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
 import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
 
-import java.awt.Image;
-import java.io.File;
-import java.io.IOException;
 
-import javax.imageio.ImageIO;
-
-
-public class MeepMeepTesting {
+public class MeepMeepStageRed_Center {
     public static void main(String[] args) {
         MeepMeep meepMeep = new MeepMeep(800);
 
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/StageRed.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/StageRed.java
@@ -46,8 +46,8 @@ public class StageRed extends LinearOpMode {
         blinkin.setPattern(pipelineNotReady);
 
         int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
-        webcam = OpenCvCameraFactory.getInstance().createWebcam(hardwareMap.get(WebcamName.class, "Red"), cameraMonitorViewId);
-        Pipeline detector = new Pipeline(telemetry, StartPosition.RED_STAGE, blinkin);
+        webcam = OpenCvCameraFactory.getInstance().createWebcam(hardwareMap.get(WebcamName.class, "Blue"), cameraMonitorViewId); //TODO: Change webcam name
+        Pipeline detector = new Pipeline(telemetry, StartPosition.RED_STAGE, blinkin); //TODO: Change start position
         webcam.setPipeline(detector);
 
         webcam.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener() {
@@ -59,8 +59,15 @@ public class StageRed extends LinearOpMode {
             @Override
             public void onError(int errorCode) {}
         });
+        felipe2.setAnglerLoad();
+        sleep(250);
+        felipe2.gripperWideOpen();
+        felipe2.slideMechanicalReset();
+        felipe2.setTurnerLoad();
+        sleep(250);
 
         waitForStart();
+
 
         detector.toggleTelemetry();
         telemetry.clearAll();
@@ -99,27 +106,84 @@ public class StageRed extends LinearOpMode {
         Pose2d startPos = new Pose2d(10,-56,Math.toRadians(180));
         Pose2d wallpark = new Pose2d(1,1,Math.toRadians(180));
 
+        //CENTER POSES
         Pose2d YellowPixelDropOff_CENTER = new Pose2d(45,-37,Math.toRadians(180));
         Pose2d PurplePixelDropOff_CENTER = new Pose2d(22,-25,Math.toRadians(180));
-        Pose2d SplinePt1 = new Pose2d(10,-56,Math.toRadians(180));
-        Pose2d SplinePt2 = new Pose2d(10,-56,Math.toRadians(180));
+
+        /**
+         * the number 1 symbolizes the journey from backstage to audience
+         * the number 2 symbolizes the journey from audience to backstage
+         * letter symbolizes the different parts of the path
+         */
+
         Pose2d WhiteTravelPart1a_CENTER = new Pose2d(-54,-36, Math.toRadians(180));
         Pose2d WhiteTravelPart2a_CENTER = new Pose2d(10,-35, Math.toRadians(180));
         Pose2d WhiteTravelPart2b_Center = new Pose2d (48,-56, Math.toRadians(180));
 
+        //LEFT POSES
+
+
+        //RIGHT POSES
+
         drive.setPoseEstimate(startPos);
 
         TrajectorySequence StageRedCenter = drive.trajectorySequenceBuilder(startPos)
+                //initialize all the subsystems and drive the purple pixel dropoff
                 .UNSTABLE_addTemporalMarkerOffset(0.0,()->{felipe2.setSlideRow_1();})
+                .UNSTABLE_addTemporalMarkerOffset(0.0,()->{felipe2.gripperClosed();})
                 .lineToLinearHeading(PurplePixelDropOff_CENTER)
+                .UNSTABLE_addTemporalMarkerOffset(0.0,()->{felipe2.gripperRightOpen();})
+                .waitSeconds(0.125)
+                .UNSTABLE_addTemporalMarkerOffset(0.0,()->{felipe2.setTurnerDeploy();})
+                .UNSTABLE_addTemporalMarkerOffset(0.0,()->{felipe2.setAnglerDeploy();})
+                //release right gripper, turn the turner, set the slide one, and angle the angler to deploy
+                //go the the backstage, to the center position
                 .lineToLinearHeading(YellowPixelDropOff_CENTER)
+                .waitSeconds(0.125)
+                .UNSTABLE_addTemporalMarkerOffset(0.0,()->{felipe2.gripperLeftOpen();})
+                .waitSeconds(0.125)
+                .back(12)
+                .UNSTABLE_addTemporalMarkerOffset(0.0,()->{felipe2.setTurnerLoad();})
+                .UNSTABLE_addTemporalMarkerOffset(0.0,()->{felipe2.setAnglerLoad();})
+                .UNSTABLE_addTemporalMarkerOffset(0.0,()->{felipe2.setSlideLevel_0();})
+                //back up and put turner to load, angler to load, slide to level 0
+                //go under the truss closest the the centerstage door
                 .lineToLinearHeading(WhiteTravelPart1a_CENTER)
+                .UNSTABLE_addTemporalMarkerOffset(0.0,()->{felipe2.setSlideRow_1();})
+                .UNSTABLE_addTemporalMarkerOffset(0.0,()->{felipe2.gripperClosed();})
+                .waitSeconds(0.125)
+                .back(10)
+                .UNSTABLE_addTemporalMarkerOffset(0.0,()->{felipe2.setSlideLevel_0();})
+                //set to slide level 1, and close gripper
+                //back up and put slide level to 0
+                //travel back to under the truss
                 .lineToLinearHeading(WhiteTravelPart2a_CENTER)
+                .strafeLeft(30)
+                .UNSTABLE_addTemporalMarkerOffset(0.0,()->{felipe2.setTurnerDeploy();})
+                .UNSTABLE_addTemporalMarkerOffset(0.0,()->{felipe2.setTurnerLoad();})
+
+                //turn the turner
+                //travel to the backstage but make an L motion to avoid crashing but drop on ground
+                //open gripper
+                //turn the turner back
+                //travel back under the truss
                 .lineToLinearHeading(WhiteTravelPart2b_Center)
+                .UNSTABLE_addTemporalMarkerOffset(0.0,()->{felipe2.gripperOpen();})
+                .waitSeconds(0.125)
+                .UNSTABLE_addTemporalMarkerOffset(0.0,()->{felipe2.setTurnerLoad();})
+                //go back to right before the truss location
                 .lineToLinearHeading(WhiteTravelPart2a_CENTER)
+                //go to white stacks and pick up 3rd and 4th white pixel
                 .lineToLinearHeading(WhiteTravelPart1a_CENTER)
+                .UNSTABLE_addTemporalMarkerOffset(0.0,()->{felipe2.setSlideRow_1();})
+                .UNSTABLE_addTemporalMarkerOffset(0.0,()->{felipe2.setTurnerLoad();})
+                //set to slide level 1, and close gripper
+                //back up and put slide level to 0
+                //travel back under the truss
                 .lineToLinearHeading(WhiteTravelPart2a_CENTER)
+                //make an L motion and travel to backstage but drop on ground
                 .lineToLinearHeading(WhiteTravelPart2b_Center)
+                //drop pixels and back up and park
                 .build();
 
 
@@ -134,7 +198,7 @@ public class StageRed extends LinearOpMode {
             webcam.closeCameraDevice();
         }
 
-        telemetry.addData("Running path", " RED_STAGE" + location);
+        telemetry.addData("Running path", " RED_STAGE" + location); //TODO: Change name in telemetry
         telemetry.update();
 
         switch(location) {
