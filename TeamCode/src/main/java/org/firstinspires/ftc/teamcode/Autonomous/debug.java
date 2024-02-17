db6e1a6181cdb4684e3eb134b2549478c124a1de
Ritika Rudrapati <ritikarudrapati@gmail.com>
2024-02-17 13:40:50 -0600

    first part of tuning

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/debug.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/debug.java
@@ -23,14 +23,8 @@ import org.openftc.easyopencv.OpenCvCameraRotation;
 
 @Autonomous
 //@Disabled
-public class StageRed24 extends LinearOpMode {
+public class debug extends LinearOpMode {
     static final double FEET_PER_METER = 3.28084;
-    OpenCvWebcam webcam;
-    RevBlinkinLedDriver blinkin;
-    RevBlinkinLedDriver.BlinkinPattern pipelineError = RevBlinkinLedDriver.BlinkinPattern.HEARTBEAT_RED;
-    RevBlinkinLedDriver.BlinkinPattern pipelineBroken = RevBlinkinLedDriver.BlinkinPattern.LARSON_SCANNER_RED;
-    RevBlinkinLedDriver.BlinkinPattern pipelineNotReady = RevBlinkinLedDriver.BlinkinPattern.BREATH_RED;
-    RevBlinkinLedDriver.BlinkinPattern pipelineReady = RevBlinkinLedDriver.BlinkinPattern.DARK_GREEN;
 
     @Override
     public void runOpMode() throws InterruptedException {
@@ -41,63 +35,6 @@ public class StageRed24 extends LinearOpMode {
         MecanumDriveBase drive = new MecanumDriveBase(hardwareMap);
 
         telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
-        blinkin = hardwareMap.get(RevBlinkinLedDriver.class, "blinkin");
-
-        blinkin.setPattern(pipelineNotReady);
-
-        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
-        webcam = OpenCvCameraFactory.getInstance().createWebcam(hardwareMap.get(WebcamName.class, "Red"), cameraMonitorViewId);
-        Pipeline detector = new Pipeline(telemetry, StartPosition.RED_STAGE, blinkin);
-        webcam.setPipeline(detector);
-
-        webcam.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener() {
-            @Override
-            public void onOpened() {
-                // this camera supports 1280x800, 1280x720, 800x600, 640x480, and 320x240
-                webcam.startStreaming(640, 480, OpenCvCameraRotation.SENSOR_NATIVE, OpenCvWebcam.StreamFormat.MJPEG);
-            }
-            @Override
-            public void onError(int errorCode) {}
-        });
-        felipe.setAnglerLoad();
-        sleep(250);
-        felipe.gripperClosed();
-        sleep(250);
-        felipe.slideMechanicalReset();
-        sleep(250);
-
-        waitForStart();
-
-
-        detector.toggleTelemetry();
-        telemetry.clearAll();
-
-        int totalTimeWaited = 0;
-        boolean pipelineRan = true;
-        if(detector.getPropLocation() == null) {
-            telemetry.addData("ERROR", "Start was pressed too soon.");
-            telemetry.update();
-            blinkin.setPattern(pipelineError);
-
-            while(detector.getPropLocation() == null && totalTimeWaited < 7000) {
-                totalTimeWaited += (webcam.getOverheadTimeMs() * 4);
-                sleep(webcam.getOverheadTimeMs() * 4L);
-            }
-            telemetry.addData("Wasted time", totalTimeWaited);
-            if(totalTimeWaited > 7000) {
-                telemetry.addData("ERROR", "The pipeline never ran.");
-                pipelineRan = false;
-                blinkin.setPattern(pipelineBroken);
-            }
-            telemetry.update();
-        }
-        else {
-            telemetry.addData("INFO", "Pipeline is running correctly");
-            telemetry.update();
-            blinkin.setPattern(pipelineReady);
-        }
-
-        drive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
         //============================
         // ALL POSE SECTION
@@ -172,10 +109,7 @@ public class StageRed24 extends LinearOpMode {
         Pose2d WhiteTravelPart2d_RIGHT = new Pose2d(45,-60, Math.toRadians(180));
 
 
-        drive.setPoseEstimate(startPos);
-
-
-        //============================
+//============================
         // RIGHT TRAJECTORY
         //============================
         TrajectorySequence StageRedRight = drive.trajectorySequenceBuilder(startPos)
@@ -339,7 +273,7 @@ public class StageRed24 extends LinearOpMode {
                 //initialize all the subsystems and drive the purple pixel dropoff
                 .UNSTABLE_addTemporalMarkerOffset(0.0,()->{felipe.gripperClosed();})
                 .UNSTABLE_addTemporalMarkerOffset(0.0,()->{felipe.setAnglerAuto();})
-                 //purple pixel journey
+                //purple pixel journey
                 .lineToLinearHeading(PurplePixelDropOff_CENTER)
                 .UNSTABLE_addTemporalMarkerOffset(0.0,()->{felipe.gripperRightOpen();})
                 .waitSeconds(0.5)
@@ -409,33 +343,33 @@ public class StageRed24 extends LinearOpMode {
                  */
                 .build();
 
+        felipe.setAnglerLoad();
+        sleep(250);
+        felipe.gripperClosed();
+        sleep(250);
+        felipe.slideMechanicalReset();
+        sleep(250);
+
+        waitForStart();
+
+
+        telemetry.clearAll();
+
+
+        drive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+
+
+        drive.setPoseEstimate(startPos);
+
+
+
+
 
         //detector.toggleTelemetry();
         //telemetry.clearAll();
 
-        Prop location = Prop.CENTER;
 
-        if(pipelineRan) {
-            location = detector.getPropLocation();
-            webcam.stopStreaming();
-            webcam.closeCameraDevice();
-        }
-
-        telemetry.addData("Running path", " RED_STAGE" + location);
-        telemetry.update();
-
-        switch(location) {
-            case LEFT:
-                drive.followTrajectorySequence(StageRedLeft);
-                break;
-            case CENTER:
-                drive.followTrajectorySequence(StageRedCenter);
-                break;
-            case RIGHT:
-                drive.followTrajectorySequence(StageRedRight);
-                break;
-            default:
-                throw new IllegalArgumentException("the code did not detect the prop at all, and it is running the default case.");
-        }
+        drive.followTrajectorySequence(StageRedCenter);
     }
 }
