51f46d7652702d1f7ea8dcbf5cf2b856a96c6b31
Ritika Rudrapati <ritikarudrapati@gmail.com>
2024-02-17 10:41:45 -0600

    added lots of comments and subsystem motions

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/StageRed22.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/StageRed22.java
@@ -99,13 +99,16 @@ public class StageRed22 extends LinearOpMode {
         drive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
         //============================
-        // POSE SECTION
+        // ALL POSE SECTION
         //============================
 
         Pose2d startPos = new Pose2d(10,-56,Math.toRadians(180));
         Pose2d wallpark = new Pose2d(1,1,Math.toRadians(180));
 
-        //CENTER POSES
+        //============================
+        // CENTER POSE SECTION
+        //============================
+
         Pose2d YellowPixelDropOff_CENTER = new Pose2d(45,-37,Math.toRadians(180));
         Pose2d PurplePixelDropOff_CENTER = new Pose2d(22,-25,Math.toRadians(180));
 
@@ -120,7 +123,10 @@ public class StageRed22 extends LinearOpMode {
         Pose2d WhiteTravelPart2a_CENTER = new Pose2d(10,-35, Math.toRadians(180));
         Pose2d WhiteTravelPart2b_Center = new Pose2d (48,-56, Math.toRadians(180));
 
-        //LEFT POSES
+        //============================
+        // LEFT POSE SECTION
+        //============================
+
         Pose2d YellowPixelDropOff_LEFT = new Pose2d(45,-29,Math.toRadians(180));
         Pose2d PurplePixelDropOff_LEFT_1a = new Pose2d(10,-35,Math.toRadians(180));
         Pose2d PurplePixelDropOff_LEFT_1b = new Pose2d(4,-35,Math.toRadians(180));
@@ -141,14 +147,129 @@ public class StageRed22 extends LinearOpMode {
         Pose2d WhiteTravelPart2c_LEFT = new Pose2d(0,-60, Math.toRadians(180));
         Pose2d WhiteTravelPart2d_LEFT = new Pose2d(45,-60, Math.toRadians(180));
 
-        //RIGHT POSES
+        //============================
+        // RIGHT POSE SECTION
+        //============================
+
+        Pose2d YellowPixelDropOff_RIGHT = new Pose2d(0,0,Math.toRadians(180));
+        Pose2d PurplePixelDropOff_RIGHT = new Pose2d(0,0,Math.toRadians(180));
+
+        /**
+         * the number 1 symbolizes the journey from backstage to audience
+         * the number 2 symbolizes the journey from audience to backstage
+         * letter symbolizes the different parts of the path
+         */
+
+        Pose2d WhiteTravelPart1a_RIGHT = new Pose2d(-0,-60, Math.toRadians(180));
+        Pose2d WhiteTravelPart1b_RIGHT = new Pose2d(-20,-60, Math.toRadians(180));
+        Pose2d WhiteTravelPart1c_RIGHT = new Pose2d(-42,-60, Math.toRadians(180));
+        Pose2d WhiteTravelPart1e_RIGHT = new Pose2d(-56,-36, Math.toRadians(180));
+
+        Pose2d WhiteTravelPart2a_RIGHT = new Pose2d(-42,-60, Math.toRadians(180));
+        Pose2d WhiteTravelPart2b_RIGHT = new Pose2d(-20,-60, Math.toRadians(180));
+        Pose2d WhiteTravelPart2c_RIGHT = new Pose2d(0,-60, Math.toRadians(180));
+        Pose2d WhiteTravelPart2d_RIGHT = new Pose2d(45,-60, Math.toRadians(180));
+
 
         drive.setPoseEstimate(startPos);
 
+
+        //============================
+        // RIGHT TRAJECTORY
+        //============================
+        TrajectorySequence StageRedRight = drive.trajectorySequenceBuilder(startPos)
+                //initialize all the subsystems and drive the purple pixel dropoff
+                .UNSTABLE_addTemporalMarkerOffset(0.0,()->{felipe2.gripperClosed();})
+                .UNSTABLE_addTemporalMarkerOffset(0.0,()->{felipe2.setAnglerAuto();})
+                /**
+                 * purple pixel journey
+                 */
+                //head over to purple pixel push prop and then come back
+                //add lines to to do right purple pixel dropoff
+                //open gripper
+                .UNSTABLE_addTemporalMarkerOffset(0.0,()->{felipe2.gripperRightOpen();})
+                //wait and back out
+                .waitSeconds(0.125)
+                .back(15)
+                /**
+                 * yellow pixel journey
+                 */
+                //initialize all the subsystems to get ready to drop the yellow pixel
+                .UNSTABLE_addTemporalMarkerOffset(0.0,()->{felipe2.setTurnerDeploy();})
+                .UNSTABLE_addTemporalMarkerOffset(0.0,()->{felipe2.setSlideRow_1();})
+                .UNSTABLE_addTemporalMarkerOffset(0.0,()->{felipe2.setAnglerDeploy();})
+                //go to yellow pixel drop off
+                //add line to linear heading to yellow pixel dropoff right
+                .lineToLinearHeading(YellowPixelDropOff_RIGHT)
+                .waitSeconds(0.125)
+                //drop pixel and head back
+                .UNSTABLE_addTemporalMarkerOffset(0.0,()->{felipe2.gripperLeftOpen();})
+                .waitSeconds(0.125)
+                .back(10)
+                /**
+                 * white pixel journey 1
+                 */
+                //set turner to load, set angler to auto, and slide level 0
+                .UNSTABLE_addTemporalMarkerOffset(0.0,()->{felipe2.setAnglerLoad();})
+                .UNSTABLE_addTemporalMarkerOffset(0.0,()->{felipe2.setTurnerLoad();})
+                .UNSTABLE_addTemporalMarkerOffset(0.0,()->{felipe2.setSlideLevel_0();})
+                //spline to white pixel stacks
+                .splineToLinearHeading(WhiteTravelPart1a_RIGHT, Math.toRadians(180))
+                .splineToLinearHeading(WhiteTravelPart1b_RIGHT,Math.toRadians(180))
+                .splineToLinearHeading(WhiteTravelPart1c_RIGHT,Math.toRadians(180))
+                .splineToLinearHeading(WhiteTravelPart1e_RIGHT, Math.toRadians(180))
+                //set turner to 11 degrees
+                .UNSTABLE_addTemporalMarkerOffset(0.0,()->{felipe2.gripperClosed();})
+                .waitSeconds(0.125)
+                .back(5)
+                //spline to backstage area
+                .splineToLinearHeading(WhiteTravelPart2a_RIGHT,Math.toRadians(180))
+                .splineToLinearHeading(WhiteTravelPart2b_RIGHT,Math.toRadians(180))
+                .splineToLinearHeading(WhiteTravelPart2c_RIGHT,Math.toRadians(180))
+                .splineToLinearHeading(WhiteTravelPart2d_RIGHT,Math.toRadians(180))
+                //set turner to deploy, set slide level to 1, open gripper
+                .UNSTABLE_addTemporalMarkerOffset(0.0,()->{felipe2.setTurnerDeploy();})
+                .UNSTABLE_addTemporalMarkerOffset(0.0,()->{felipe2.setSlideRow_1();})
+                .waitSeconds(0.125)
+                .UNSTABLE_addTemporalMarkerOffset(0.0,()->{felipe2.gripperOpen();})
+                .waitSeconds(0.125)
+                /**
+                 * white pixel journey 2
+                 */
+                //set turner to load again, set slide level 0
+                .UNSTABLE_addTemporalMarkerOffset(0.0,()->{felipe2.setTurnerLoad();})
+                .UNSTABLE_addTemporalMarkerOffset(0.0,()->{felipe2.setSlideLevel_0();})
+                //spline to white stacks again
+                .splineToLinearHeading(WhiteTravelPart1a_RIGHT, Math.toRadians(180))
+                .splineToLinearHeading(WhiteTravelPart1b_RIGHT,Math.toRadians(180))
+                .splineToLinearHeading(WhiteTravelPart1c_RIGHT,Math.toRadians(180))
+                .splineToLinearHeading(WhiteTravelPart1e_RIGHT, Math.toRadians(180))
+                //set turner to 11 degrees
+                //grip and back out
+                .UNSTABLE_addTemporalMarkerOffset(0.0,()->{felipe2.gripperClosed();})
+                .waitSeconds(0.125)
+                .back(5)
+                //spline to backstage area again
+                .splineToLinearHeading(WhiteTravelPart2a_RIGHT,Math.toRadians(180))
+                .splineToLinearHeading(WhiteTravelPart2b_RIGHT,Math.toRadians(180))
+                .splineToLinearHeading(WhiteTravelPart2c_RIGHT,Math.toRadians(180))
+                .splineToLinearHeading(WhiteTravelPart2d_RIGHT,Math.toRadians(180))
+                //drop pixels, and back out
+                .UNSTABLE_addTemporalMarkerOffset(0.0,()->{felipe2.gripperOpen();})
+                .back(25)
+                .build();
+
+
+        //============================
+        // LEFT TRAJECTORY
+        //============================
         TrajectorySequence StageRedLeft = drive.trajectorySequenceBuilder(startPos)
                 //initialize all the subsystems and drive the purple pixel dropoff
                 .UNSTABLE_addTemporalMarkerOffset(0.0,()->{felipe2.gripperClosed();})
                 .UNSTABLE_addTemporalMarkerOffset(0.0,()->{felipe2.setAnglerAuto();})
+                /**
+                 * purple pixel journey
+                 */
                 //head over to purple pixel push prop and then come back
                 .lineToLinearHeading(PurplePixelDropOff_LEFT_1a)
                 .lineToLinearHeading(PurplePixelDropOff_LEFT_1b)
@@ -157,6 +278,9 @@ public class StageRed22 extends LinearOpMode {
                 //wait and back out
                 .waitSeconds(0.125)
                 .back(15)
+                /**
+                 * yellow pixel journey
+                 */
                 //initialize all the subsystems to get ready to drop the yellow pixel
                 .UNSTABLE_addTemporalMarkerOffset(0.0,()->{felipe2.setTurnerDeploy();})
                 .UNSTABLE_addTemporalMarkerOffset(0.0,()->{felipe2.setSlideRow_1();})
@@ -168,6 +292,9 @@ public class StageRed22 extends LinearOpMode {
                 .UNSTABLE_addTemporalMarkerOffset(0.0,()->{felipe2.gripperLeftOpen();})
                 .waitSeconds(0.125)
                 .back(10)
+                /**
+                 * white pixel journey 1
+                 */
                 //strafe left to avoid conflict
                 .strafeLeft(15)
                 //set turner to load, set angler to auto, and slide level 0
@@ -194,6 +321,9 @@ public class StageRed22 extends LinearOpMode {
                 .waitSeconds(0.125)
                 .UNSTABLE_addTemporalMarkerOffset(0.0,()->{felipe2.gripperOpen();})
                 .waitSeconds(0.125)
+                /**
+                 * white pixel journey 2
+                 */
                 //set turner to load again, set slide level 0
                 .UNSTABLE_addTemporalMarkerOffset(0.0,()->{felipe2.setTurnerLoad();})
                 .UNSTABLE_addTemporalMarkerOffset(0.0,()->{felipe2.setSlideLevel_0();})
@@ -217,10 +347,16 @@ public class StageRed22 extends LinearOpMode {
                 .back(25)
                 .build();
 
+        //============================
+        // CENTER TRAJECTORY
+        //============================
         TrajectorySequence StageRedCenter = drive.trajectorySequenceBuilder(startPos)
                 //initialize all the subsystems and drive the purple pixel dropoff
                 .UNSTABLE_addTemporalMarkerOffset(0.0,()->{felipe2.gripperClosed();})
                 .UNSTABLE_addTemporalMarkerOffset(0.0,()->{felipe2.setAnglerAuto();})
+                /**
+                 * purple pixel journey
+                 */
                 .lineToLinearHeading(PurplePixelDropOff_CENTER)
                 .UNSTABLE_addTemporalMarkerOffset(0.0,()->{felipe2.gripperRightOpen();})
                 .waitSeconds(0.125)
@@ -228,11 +364,17 @@ public class StageRed22 extends LinearOpMode {
                 .UNSTABLE_addTemporalMarkerOffset(0.0,()->{felipe2.setAnglerDeploy();})
                 //release right gripper, turn the turner, set the slide one, and angle the angler to deploy
                 //go the the backstage, to the center position
+                /**
+                 * yellow pixel journey
+                 */
                 .lineToLinearHeading(YellowPixelDropOff_CENTER)
                 .waitSeconds(0.125)
                 .UNSTABLE_addTemporalMarkerOffset(0.0,()->{felipe2.gripperLeftOpen();})
                 .waitSeconds(0.125)
                 .back(12)
+                /**
+                 * white pixel journey 1
+                 */
                 .UNSTABLE_addTemporalMarkerOffset(0.0,()->{felipe2.setTurnerLoad();})
                 .UNSTABLE_addTemporalMarkerOffset(0.0,()->{felipe2.setAnglerLoad();})
                 .UNSTABLE_addTemporalMarkerOffset(0.0,()->{felipe2.setSlideLevel_0();})
@@ -261,6 +403,9 @@ public class StageRed22 extends LinearOpMode {
                 .lineToLinearHeading(WhiteTravelPart2b_Center)
                 .UNSTABLE_addTemporalMarkerOffset(0.0,()->{felipe2.gripperOpen();})
                 .waitSeconds(0.125)
+                /**
+                 * white pixel journey 2
+                 */
                 .UNSTABLE_addTemporalMarkerOffset(0.0,()->{felipe2.setTurnerLoad();})
                 //go back to right before the truss location
                 .lineToLinearHeading(WhiteTravelPart2a_CENTER)
@@ -300,16 +445,16 @@ public class StageRed22 extends LinearOpMode {
 
         switch(location) {
             case LEFT:
-                sleep(5000);
+                drive.followTrajectorySequence(StageRedLeft);
                 break;
             case CENTER:
-                sleep(5000);
+                drive.followTrajectorySequence(StageRedCenter);
                 break;
             case RIGHT:
-                sleep(5000);
+                drive.followTrajectorySequence(StageRedRight);
                 break;
             default:
-                throw new IllegalArgumentException("The code is most certainly severely screwed up.");
+                throw new IllegalArgumentException("the code did not detect the prop at all, and it is running the default case.");
         }
     }
 }
